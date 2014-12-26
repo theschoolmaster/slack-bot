feebe599b602b8e1c83744f1061cbf1c536c611d
audibleblink <virtualsnyper@gmail.com>
2014-12-26 14:55:34 -0500

    spacing and semicolon pass

--- bot.js
+++ bot.js
@@ -1,7 +1,8 @@
+var botID   = process.env.BOT_ID
 var apiKey  = process.env.XBOX_API
-var xboxApi = require('node-xbox')(apiKey);
-var HTTPS = require('https');
-var texts = [
+var xboxApi = require('node-xbox')(apiKey)
+var HTTPS   = require('https')
+var texts   = [
     'Are you coming? My battery ubisnabkit to do above the tondo. And',
     'Fish',
     'Can your poco me up',
@@ -11,99 +12,96 @@ var texts = [
     'U Jane fps for toy',
     'Gosh',
     'My battery us dying'
-  ]
+]
 
-var botID = process.env.BOT_ID;
 
 function respond() {
-  var request = JSON.parse(this.req.chunks[0])
+    var request = JSON.parse(this.req.chunks[0])
 
-  if(request.text){
+    if (request.text) {
 
-    // check status of XBoxLive user
-    if(RegExp("^!live (.+)", 'i').test(request.text)) {
-      var gamertag = RegExp("^!live (.+)", 'i').exec(request.text)[1];
-      var that = this
-      xboxApi.profile.xuid(gamertag, function(err, returnedXuid){ 
-        xboxApi.profile.presence(returnedXuid, function(err, returnedPresence){
-          var returnedPresence = JSON.parse(returnedPresence)
-          var response = gamertag + " is " + returnedPresence.state + "\n"
+        // check status of XBoxLive user
+        if (RegExp("^!live (.+)", 'i').test(request.text)) {
+            var gamertag = RegExp("^!live (.+)", 'i').exec(request.text)[1]
+            var that = this
+            xboxApi.profile.xuid(gamertag, function(err, returnedXuid) {
+                xboxApi.profile.presence(returnedXuid, function(err, returnedPresence) {
+                    var returnedPresence = JSON.parse(returnedPresence)
+                    var response = gamertag + " is " + returnedPresence.state + "\n"
 
-          if(returnedPresence.state === "Offline"){
-            response += "Last seen: "
-            response += returnedPresence.lastSeen.timestamp + " "
-            response += "Playing: "
-            response += returnedPresence.lastSeen.titleName
-          } else if(returnedPresence.state === "Online") {
-            response += "Playing: "
-            response += returnedPresence.devices[0].titles[1].name
-          }
+                    if (returnedPresence.state === "Offline") {
+                        response += "Last seen: "
+                        response += returnedPresence.lastSeen.timestamp + " "
+                        response += "Playing: "
+                        response += returnedPresence.lastSeen.titleName
+                    } else if (returnedPresence.state === "Online") {
+                        response += "Playing: "
+                        response += returnedPresence.devices[0].titles[1].name
+                    }
 
-          that.res.writeHead(200);
-          postMessage(response);
-          that.res.end();        
-        })
-      })
+                    that.res.writeHead(200)
+                    postMessage(response)
+                    that.res.end()
+                })
+            })
+        }
+
+        // Resonse to "hello nawbot"
+        if (RegExp("^hello nawbot$", 'i').test(request.text)) {
+            this.res.writeHead(200)
+            postMessage("Very nice of you to think of me.  I was starting to feel neglected")
+            this.res.end()
+        }
+
+        // Logan's drunk texts
+        if (RegExp("^text logan$", 'i').test(request.text)) {
+            rand = Math.floor(Math.random() * texts.length)
+            this.res.writeHead(200)
+            postMessage(texts[rand])
+            this.res.end()
+        } else {
+            console.log("don't care")
+            this.res.writeHead(200)
+            this.res.end()
+        }
     }
-
-    // Resonse to "hello nawbot"
-    if(RegExp("^hello nawbot$", 'i').test(request.text)) {
-      this.res.writeHead(200);
-      postMessage("Very nice of you to think of me.  I was starting to feel neglected");
-      this.res.end();
-    } 
-
-    // Logan's drunk texts
-    if(RegExp("^text logan$", 'i').test(request.text)) {
-      rand = Math.floor(Math.random() * texts.length ) 
-      this.res.writeHead(200);
-      postMessage(texts[rand]);
-      this.res.end();
-    } 
-
-    else {
-      console.log("don't care");
-      this.res.writeHead(200);
-      this.res.end();
-    }
-  }
 
 }
 
 function postMessage(msg) {
-  var botResponse, options, body, botReq;
+    var botResponse, options, body, botReq
 
-  botResponse = msg
+    botResponse = msg
 
-  options = {
-    hostname: 'api.groupme.com',
-    path: '/v3/bots/post',
-    method: 'POST'
-  };
+    options = {
+        hostname: 'api.groupme.com',
+        path: '/v3/bots/post',
+        method: 'POST'
+    }
 
-  body = {
-    "bot_id" : botID,
-    "text" : botResponse
-  };
+    body = {
+        "bot_id": botID,
+        "text": botResponse
+    }
 
-  console.log('sending ' + botResponse + ' to ' + botID);
+    console.log('sending ' + botResponse + ' to ' + botID)
 
-  botReq = HTTPS.request(options, function(res) {
-      if(res.statusCode == 202) {
-        //neat
-      } else {
-        console.log('rejecting bad status code ' + res.statusCode);
-      }
-  });
+    botReq = HTTPS.request(options, function(res) {
+        if (res.statusCode == 202) {
+            //neat
+        } else {
+            console.log('rejecting bad status code ' + res.statusCode)
+        }
+    })
 
-  botReq.on('error', function(err) {
-    console.log('error posting message '  + JSON.stringify(err));
-  });
-  botReq.on('timeout', function(err) {
-    console.log('timeout posting message '  + JSON.stringify(err));
-  });
-  botReq.end(JSON.stringify(body));
+    botReq.on('error', function(err) {
+        console.log('error posting message ' + JSON.stringify(err))
+    })
+    botReq.on('timeout', function(err) {
+        console.log('timeout posting message ' + JSON.stringify(err))
+    })
+    botReq.end(JSON.stringify(body))
 }
 
 
-exports.respond = respond;
+exports.respond = respond
