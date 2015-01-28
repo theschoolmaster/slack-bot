59c22965d31927ba77dcffbd42ac456652fc9710
audibleblink <virtualsnyper@gmail.com>
2015-01-28 16:45:32 -0500

    use slack's api which takes a response body
    - remove #postMessage; no need to POST, just reply

--- bot.js
+++ bot.js
@@ -18,15 +18,13 @@ function respond() {
 
     if (request.text) {
         if (keyword == "!live") {
-            var gamertag = message.replace(RegExp(keyword + " "), "")
+            gamertag = message.replace(RegExp(keyword + " "), "")
             XBL.getXuid(gamertag)
                 .then(XBL.getPresence)
                 .then(XBL.prepareResponse)
                 .then(function(response){
                     // console.log(response)
-                    self.res.writeHead(200)
-                    postMessage(response, sourceChannel)
-                    self.res.end()
+                    replytWith(self, response)
                 })
                 .catch(function(error) {
                     console.log(error)
@@ -34,48 +32,15 @@ function respond() {
         }
 
         if (keyword == "!ping") {
-            this.res.writeHead(200, { 'Content-Type': 'application/json' })
-            this.res.end('{"text": "pong!"}')
+            replytWith(this, "pong!")
         }
     }
 }
 
-
-function postMessage(msg, channel) {
-    var botResponse, options, body, botReq
-
-    botResponse = msg
-
-    options = {
-        hostname: 'hooks.slack.com',
-        path: '/services/'+ slackTeam +'/B03E4RPMW/' + slackToken,
-        method: 'POST'
-    }
-
-    body = {
-        "channel": "#" + channel,
-        "username": "xblBot",
-        "text": botResponse,
-        "icon_emoji": ":video_game:"
-    }
-
-    console.log('sending ' + botResponse + ' to Slack channel: #' + channel)
-
-    botReq = HTTPS.request(options, function(res) {
-        if (res.statusCode == 200) {
-            //neat
-        } else {
-            console.log('rejecting bad status code ' + res.statusCode)
-        }
-    })
-
-    botReq.on('error', function(err) {
-        console.log('error posting message ' + JSON.stringify(err))
-    })
-    botReq.on('timeout', function(err) {
-        console.log('timeout posting message ' + JSON.stringify(err))
-    })
-    botReq.end(JSON.stringify(body))
+function replytWith(context, body) {
+    context.res.writeHead(200, { 'Content-Type': 'application/json' })
+    context.res.end('{"text": "' + body + '"}')
 }
+
 
 exports.respond = respond
