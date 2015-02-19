8c5c56cd30f2a958b37197dd2a982b02385b42e7
audibleblink <virtualsnyper@gmail.com>
2015-02-19 15:51:17 -0500

    config variable naming was confusing

--- imageSearch.js
+++ imageSearch.js
@@ -1,10 +1,10 @@
 var client = require("google-images2")
 
-function retrieveImages(queryString, channel, cb){
+function retrieveImages(queryString, options, cb){
 
     client.search(queryString, function(err, images){
         images.forEach(function(obj){
-            cb(obj.url, channel)
+            cb(obj.url, options)
         })
     })
 
