e076f44d7fdfbd5b0ee33364cc69c836613d763e
audibleblink <virtualsnyper@gmail.com>
2015-04-13 15:31:22 -0400

    strip anything after jpg, gif and png file extensions

--- imageSearch.js
+++ imageSearch.js
@@ -3,7 +3,8 @@ var client = require("google-images2")
 function retrieveImages(queryString, cb){
 
     client.search(queryString, function(err, images){
-        err ? cb.call(this, "error") : cb.call(this, images[0].url)
+        var imageUrl = images[0].url.replace(/(\.jpeg|\.jpg|\.gif|\.png)(.+)?/, '$1')
+        err ? cb.call(this, "error") : cb.call(this, imageUrl)
     }.bind(this))
 
 }
